aa7178ee35b9a2fd56d35547323aca83fe70f516
Sylvain Galtier <40130263+SGALT@users.noreply.github.com>
2023-10-26 15:16:13 +0200

    Add files via upload

--- js/qrscanner.js
+++ js/qrscanner.js
@@ -5,13 +5,30 @@ const form = document.querySelector("form");
 const inputId = document.querySelector("#id");
 const camQrResult = document.getElementById('cam-qr-result');
 
-const submitQrCode = (label) => {
-  form.submit();
+const submitQrCode = (input) => {
+  const url = "https://hook.eu1.make.com/6rry6qjjmyeocqrrxp3mseu6hjhygnp2"
+  const data = {
+    id: input.value
+  }
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)  // Convertir les données en chaîne JSON
+  })
+    .then(response => response.json())  // Si le serveur renvoie du JSON
+    .then(data => {
+      console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
 }
 
 function setResult(label, result) {
   label.value = result.data;
-  submitQrCode();
+  submitQrCode(label);
 }
 
 const scanner = new QrScanner(video, result => setResult(inputId, result), {
